d11ed598e40f3190669daefe326aa13f01778f38
lafebre_dev <lafebre610@gmail.com>
2026-07-10 20:30:48 -0500

    pruebas finales en el celular

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-//musica
+//===========================
+// VARIABLES
+//===========================
+let intervaloFade = null;
+let reproduciedo = false;
+let contadorNoTocar = 0;
 
-// configuracion de la fecha de cumpleaños
+const mensajesNoTocar = [
+    "😶 Te dije queno lo tocaras...",
+    "🤨 ¿Otra vez?",
+    "😂 Ya sabía que ibas a volver.",
+    "🙄 Bueno... ya que estás aquí.",
+    "🎉 ¡Feliz cumpleaños otra vez! ❤️"
+
+];
+
+const audioHTML = document.getElementById("fondo-musica");
+const boton = document.getElementById("boton-musica");
+const iconoPath = document.getElementById("icono-path");
 const fechaCumple = new Date().getTime() - 1000;
+//const fechaCumple = new Date("2026-07-11T00:00:00").getTime();
+
+//===========================
+// RELOG
+//===========================
 
 function actualizarReloj() {
     const ahora = new Date().getTime();
@@ -11,7 +32,9 @@ function actualizarReloj() {
         clearInterval(intervalo);
         document.getElementById("seccion-reloj").style.display = "none";
         document.getElementById("contenido-regalo").style.display = "block";
-        lanzarConfeti();
+        document.getElementById("boton-musica").style.display = "flex";
+        
+        mostrarRegalo();
         return;
     }
 
@@ -23,15 +46,13 @@ function actualizarReloj() {
     document.getElementById("reloj").innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
 }
 
-const intervalo = setInterval(actualizarReloj, 1000);
-actualizarReloj();
-// iniciar y ejecutar el reloj cada segundo
 
-const cartas = document.querySelectorAll(".contenido-carta");
-console.log("Cantidad de cartas:", cartas.length);
+//===========================
+// INICIALIAZACION           
+//===========================
+    const cartas = document.querySelectorAll(".contenido-carta");
     cartas.forEach(parrafo => {
         const texto = parrafo.textContent.trim();
-        console.log("Texto:", texto);
         parrafo.dataset.texto = texto;
         parrafo.textContent = "";
         parrafo.dataset.indice = 0;
@@ -51,105 +72,61 @@ console.log("Cantidad de cartas:", cartas.length);
         parrafo.appendChild(cursor);
 
     });
-    
-// funcion para lanzar confeti
+
+function inicializarEventos(){
+    document
+    .getElementById("boton-ultimo-regalo")
+    .addEventListener("click", abrirUltimoRegalo);
+
+    document
+    .getElementById("boton-no-tocar")
+    .addEventListener("click",botonNoTocar);
+}
+
+// iniciar relog
+const intervalo = setInterval(actualizarReloj, 1000);
+actualizarReloj();
+inicializarEventos();
+
+//=================================
+
+// EVENTOS VISUALES
+//=================================
 function lanzarConfeti() {
-    confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 }
+     confetti({
+
+        particleCount: 250,
+        spread: 360,
+        startVelocity: 45,
+        origin: {
+            x: 0.5,
+            y: 0.5
+        }
+
     });
 }
 
-// control del boton de reproducir musica
-function controlarMusica() {
-    console.log("SE EJECUTÓ controlarMusica");
-    const audioHTML = document.getElementById("fondo-musica");
-    const boton = document.getElementById("boton-musica");
-    console.log(audioHTML.paused);
-    console.log(audioHTML.currentTime);
-    console.log(audioHTML.readyState);
+function mostrarRegalo() {
+    const header = document.getElementById("header-regalo");
+    const primeraCarta = document.querySelector(".carta");
+    const retrasoInicial = 800;
+    const intervalo = 600;
+    const direcciones = ["izquierda", "derecha","arriba","abajo"];
 
-    if (!audioHTML) return;
-
-    if (audioHTML.paused) {
-        audioHTML.play()
-            .then(() => boton.textContent = "⏸️")
-            .catch(err => {
-            console.error(err);
-            alert("toca nuevamente el boton para iniciar la musica");
-        });
-    } else {
-        audioHTML.pause();
-        boton.textContent = "🎵";
-    }
+    // aparece el encabesado
+    setTimeout(() => {
+        header.classList.remove("oculto");
+        header.classList.add("aparecer");
+    }, 150);
+    
+    // aparece la carta
+    setTimeout(() => {
+        primeraCarta.classList.remove("oculto");
+        primeraCarta.classList.add("aparecer");
+        
+    }, 800);   
+    
 }
-// efecto de apertura y maquina de escribir version 4.4   
-function abrirCarta(elemento) {
-
-    const parrafo = elemento.querySelector(".contenido-carta");
-    const carta = elemento;
-    const textoSpan = parrafo.querySelector(".texto");
-    const cursor = parrafo.querySelector(".cursor");
-    if (!parrafo) return;
-
-    // si esta abrierta se cierra
-    if (parrafo.classList.contains("abierta")){
-        parrafo.classList.remove("abierta");
-        carta.classList.remove("abierta");
-        cursor.style.display = "none";
-        return;
-    }
-
-    // si esta cerrada la abrimos 
-    parrafo.classList.add("abierta");
-    carta.classList.add("abierta");
-    cursor.style.display = "inline";
-    
-    const textoCompleto = parrafo.dataset.texto;
-    console.log(textoCompleto);
-    let i = Number(parrafo.dataset.indice);
-    console.log(i);
-    textoSpan.textContent = textoCompleto.slice(0,i);
-    const velocidadBase = 45;
-    let ultimoTiempo = 0;
-    let velocidadActual = velocidadBase;
-  
-
-   
-    function bucleEscritura(tiempoActual) {
-    
-            if (!parrafo.classList.contains("abierta")) {
-                return;
-            }
-            if(!ultimoTiempo){
-                ultimoTiempo = tiempoActual;
-            }
-
-            const delta = tiempoActual - ultimoTiempo;
-            if (delta >= velocidadActual) {
-                console.log("Escribiendo..",i);
-                if (i < textoCompleto.length){
-                    textoSpan.textContent += textoCompleto[i];
-                    i++;
-                    parrafo.dataset.indice = i;
-                    ultimoTiempo = tiempoActual;
-                    velocidadActual = 35 + Math.random() * 30;
-                } else {
-                    parrafo.dataset.indice = 0;
-                    textoSpan.textContent = textoCompleto;
-                    cursor.style.display = "none";
-                return;
-                }      
-            }
-            requestAnimationFrame(bucleEscritura);      
-       
-    
-        }
-        requestAnimationFrame(bucleEscritura);
-
-}
-
 
 // fondo animado
 const fondo = document.querySelector(".fondo-animado");
@@ -170,5 +147,356 @@ const fondo = document.querySelector(".fondo-animado");
 
         fondo.appendChild(particula);
 
+    
+}
+
+
+//=================================
+//  ICONOS
+//=================================
+
+const iconoPlay =  `
+<svg id="icono-musica"
+xmlns="http://www.w3.org/2000/svg"
+width="24"
+height="24"
+fill="white"
+viewBox="0 0 24 24">
+<path d="M8 5v14l11-7z"/>
+</svg>`;
+
+const iconoPause = `
+<svg id="icono-musica"
+xmlns="http://www.w3.org/2000/svg"
+width="24"
+height="24"
+fill="white"
+viewBox="0 0 24 24">
+<path d="M6 5h4v14H6zm8 0h4v14h-4z"/>
+</svg>`;
+
+
+//====================================
+// FADE IN / FADE OUT
+//====================================
+
+function fadeIn(audio){
+
+    if (intervaloFade !== null){
+        clearInterval(intervaloFade);
+        intervaloFade = null;
     }
 
+    audio.volume = 0;
+    audio.play();
+
+    intervaloFade = setInterval(() => {
+        if (audio.volume + 0.03 >= 1){
+            audio.volume = 1;
+            clearInterval(intervaloFade);
+            intervaloFade = null;
+            
+            if (audio.onFadeEnd) {
+                audio.onFadeEnd();
+                audio.onFadeEnd = null;
+            }
+        } else {
+            audio.volume += 0.03;
+        }
+    }, 30);
+}
+
+// fade Out
+function fadeOut(audio) {
+
+        if (intervaloFade !== null) {
+            clearInterval(intervaloFade);
+            intervaloFade = null;
+        }
+         
+        intervaloFade = setInterval(() => {
+        
+        if (audio.volume - 0.03 <= 0) {
+            audio.volume = 0;
+            audio.pause();
+            clearInterval(intervaloFade);
+            intervaloFade = null;
+
+            if (audio.onFadeEnd) {
+                audio.onFadeEnd();
+                audio.onFadeEnd = null;
+            }
+        }else{
+            audio.volume -= 0.03;
+        }
+    }, 30);
+}
+
+
+//====================================
+// CONTROL DEL REPRODUCTOR
+//====================================
+
+function controlarMusica() {
+    
+    if (!audioHTML) return; 
+    
+    boton.classList.remove("pop");
+    void boton.offsetWidth;
+    boton.classList.add("pop");
+
+    if (!reproduciedo) {
+        
+            iconoPath.setAttribute("d","M6 5h4v14H6zm8 0h4v14h-4z");
+            boton.classList.add("reproduciendo");
+            
+        fadeIn(audioHTML);
+        reproduciedo = true;    
+    } else {
+        
+            iconoPath.setAttribute("d", "M8 5v14l11-7z");
+            boton.classList.remove("reproduciendo");                        
+        
+        fadeOut(audioHTML);
+        reproduciedo = false;
+    }
+}        
+     
+
+//==========================================
+// EFECTO DE MAQUINA DE ESCRIBIR
+//==========================================
+function abrirCarta(elemento) {
+
+    const carta = elemento;
+    const parrafo = carta.querySelector(".contenido-carta");
+
+    if (!parrafo) return;
+
+    const textoSpan = parrafo.querySelector(".texto");
+    const cursor = parrafo.querySelector(".cursor");
+
+    const esUltimaCarta = carta.classList.contains("carta_premio");
+
+    // Si ya está abierta, la cerramos
+    if (parrafo.classList.contains("abierta")) {
+        parrafo.classList.remove("abierta");
+        carta.classList.remove("abierta");
+        cursor.style.display = "none";
+        return;
+    }
+
+    // Abrimos la carta
+    parrafo.classList.add("abierta");
+    carta.classList.add("abierta");
+    cursor.style.display = "inline";
+
+    const textoCompleto = parrafo.dataset.texto;
+    let i = Number(parrafo.dataset.indice);
+
+    textoSpan.textContent = textoCompleto.slice(0, i);
+
+    const velocidadBase = 45;
+    let velocidadActual = velocidadBase;
+    let ultimoTiempo = 0;
+
+    function bucleEscritura(tiempoActual) {
+
+        // Si el usuario cerró la carta antes de terminar
+        if (!parrafo.classList.contains("abierta")) {
+            return;
+        }
+
+        if (!ultimoTiempo) {
+            ultimoTiempo = tiempoActual;
+        }
+
+        const delta = tiempoActual - ultimoTiempo;
+
+        if (delta >= velocidadActual) {
+
+            if (i < textoCompleto.length) {
+
+                textoSpan.textContent += textoCompleto[i];
+                i++;
+
+                parrafo.dataset.indice = i;
+
+                ultimoTiempo = tiempoActual;
+                velocidadActual = 35 + Math.random() * 30;
+
+            } else {
+
+                // Terminó de escribir
+                parrafo.dataset.indice = 0;
+                textoSpan.textContent = textoCompleto;
+                cursor.style.display = "none";
+
+                if (!esUltimaCarta) {
+
+                    const siguienteCarta = carta.nextElementSibling;
+
+                    if (
+                        siguienteCarta &&
+                        siguienteCarta.classList.contains("carta")
+                    ) {
+
+                        setTimeout(() => {
+
+                            siguienteCarta.classList.remove("oculto");
+                            siguienteCarta.classList.add("aparecer");
+
+                            siguienteCarta.classList.remove("pop");
+                            void siguienteCarta.offsetWidth;
+                            siguienteCarta.classList.add("pop");
+
+                        }, 300);
+
+                    }
+
+                } else {
+
+                    setTimeout(() => {
+
+                        const botonRegalo =
+                            document.getElementById("boton-ultimo-regalo");
+
+                        botonRegalo.classList.remove("oculto");
+                        botonRegalo.classList.add("aparecer");
+
+                        botonRegalo.classList.remove("pop");
+                        void botonRegalo.offsetWidth;
+                        botonRegalo.classList.add("pop");
+
+                    }, 1200);
+
+                }
+
+                return;
+            }
+
+        }
+
+        requestAnimationFrame(bucleEscritura);
+
+    }
+
+    requestAnimationFrame(bucleEscritura);
+
+}
+
+//=========================
+// EVENTOS
+//=========================
+audioHTML.onended =() => {
+        reproduciedo = false;
+        const boton = document.getElementById("boton-musica");
+        const iconoPath = document.getElementById("icono-path");
+
+    iconoPath.setAttribute("d", "M8 5v14l11-7z");
+    boton.classList.remove("reproduciendo");
+};
+ 
+//=======================================
+// FUNCIONES DEL ULTIMO REGAL0
+//=======================================
+function abrirUltimoRegalo() {
+    
+    const boton = document.getElementById("boton-ultimo-regalo");
+    const sobre = document.getElementById("sobre-regalo");
+    const sobreAnimado= sobre.querySelector(".sobre");
+    
+    boton.classList.remove("pop");
+    void boton.offsetWidth;
+    boton.classList.add("pop")
+
+    setTimeout(() => {
+        boton.classList.add("ocultar-boton");
+    },200);
+    
+    setTimeout(() => {
+        sobre.style.display = "block";
+        sobre.classList.add("aparecer");
+    },500);
+
+    setTimeout(() => {
+        sobreAnimado.classList.remove("pop");
+        void sobreAnimado.offsetWidth;
+        sobreAnimado.classList.add("pop");
+    },650);
+
+    setTimeout(() => {
+        sobreAnimado.classList.add("abierto");
+    },1400);
+
+    setTimeout(() => {
+        const vale = document.getElementById("vale-compensacion");
+        vale.classList.add("mostrar");
+    },1800);
+
+    setTimeout(() => {
+        sobreAnimado.classList.add("descansar")
+    },3200);
+   setTimeout(() => {
+
+    const vale = document.getElementById("vale-compensacion");
+
+    vale.classList.add("balanceo");
+
+    },3400); 
+    
+    // Mostrar botón "No tocar" después de una pequeña pausa
+    setTimeout(() => {
+        const botonNoTocar = document.getElementById("boton-no-tocar");
+
+        botonNoTocar.style.display = "flex";
+
+        botonNoTocar.classList.remove("oculto");
+        botonNoTocar.classList.add("visible");
+        botonNoTocar.classList.add("aparecer");
+
+        botonNoTocar.classList.remove("pop");
+        void botonNoTocar.offsetWidth;
+        botonNoTocar.classList.add("pop");
+    },4200);
+
+}
+
+
+function botonNoTocar(){
+
+    contadorNoTocar++;
+
+    const dialogo = document.getElementById("dialogo-no-tocar");
+
+    dialogo.textContent =
+        mensajesNoTocar[
+            Math.min(
+                contadorNoTocar-1,
+                mensajesNoTocar.length-1
+            )
+        ];
+    dialogo.classList.remove("oculto");
+    dialogo.classList.remove("aparecer");
+    void dialogo.offsetWidth;
+    dialogo.classList.add("aparecer");
+
+    clearTimeout(dialogo.timer);
+
+    dialogo.timer = setTimeout(() => {
+
+        dialogo.classList.remove("aparecer");
+        dialogo.classList.add("oculto");
+
+    },2000);
+
+    if(contadorNoTocar===5){
+
+        lanzarConfeti();
+
+        document.getElementById("boton-no-tocar").textContent =
+        "😂 Ya eres feliz, ¿no?";
+
+    }
+
+}
